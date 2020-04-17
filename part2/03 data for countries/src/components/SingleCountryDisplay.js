2630ad0379be690142f5a5316aeecdb1cca58bf6
KochsA <Andi-Kochs@web.de>
2020-04-17 20:36:44 +0200

    implemented weather display

--- part2/03 data for countries/src/components/SingleCountryDisplay.js
+++ part2/03 data for countries/src/components/SingleCountryDisplay.js
@@ -4,17 +4,25 @@ import WeatherDisplay from './WeatherDisplay'
 // displays information of a Country object
 
 const SingleCountryDisplay = ({country}) => {
+
+    const name = country.name
+    const capital = country.capital
+    const population = country.population
+    const flag = country.flag
+    const languages = country.languages
+    
+
     return(
         <div>
-            <h1>{country.name}</h1>
-            <p>capital {country.capital}</p>
-            <p>population {country.population}</p>
+            <h1>{name}</h1>
+            <p>capital {capital}</p>
+            <p>population {population}</p>
             <h2>languages</h2>
             <ul>
-                {country.languages.map(language => <li key={language.iso639_1}>{language.name}</li>)}
+                {languages.map(language => <li key={language.iso639_1}>{language.name}</li>)}
             </ul>
-            <img src={country.flag} alt=""/>
-            <WeatherDisplay countryName={country.name}/>
+            <img src={flag} width="300" height="200" alt=""/>
+            <WeatherDisplay cityName={capital}/>
         </div>
     )
 }
--- part2/03 data for countries/src/components/WeatherDisplay.js
+++ part2/03 data for countries/src/components/WeatherDisplay.js
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const WeatherDisplay = ({countryName}) => {
-    return(
-        <div>
-            <h3>Weather in {countryName}</h3>
-            <p><b>temperature: </b>° Celsius</p>
-            <img src='' alt='weather'></img>
-            <p><b>wind: </b>mph direction </p>
-        </div>
-    )
+const WeatherDisplay = ({cityName}) => {
+    const [weatherData, setWeatherData] = useState([])
+    const [hasWeatherData, setHasWeatherData] = useState(false)
+
+    const api_key = process.env.REACT_APP_API_KEY
+    const requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + api_key
+
+
+    const hook = () => {
+        axios
+            .get(requestURL)
+            .then(response => {      
+                console.log(response.data);
+                
+                setHasWeatherData(true)
+                setWeatherData( {temperature: response.data.main.temp,
+                                icon: "http://openweathermap.org/img/wn/" + response.data.weather[0].icon + "@2x.png",
+                                iconDescription: response.data.weather[0].description,
+                                windSpeed: response.data.wind.speed,
+                                windDirection: response.data.wind.deg})
+            })
+    }
+    useEffect(hook, [])
+
+    console.log(weatherData);
+    
+
+    if (hasWeatherData) {
+        return (
+                <div>
+                    <h3>Weather in {cityName}</h3>
+                    <p><b>temperature: </b>{weatherData.temperature}° Celsius</p>
+                    <img src={weatherData.icon} alt={weatherData.iconDescription}></img>
+                    <p><b>wind: </b>{weatherData.windSpeed} m/s direction {weatherData.windDirection}°</p>
+                </div>
+        )
+    }
+    else {
+        return null
+    }
+
 }
-
 
 export default WeatherDisplay
